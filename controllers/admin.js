2c6bdcf36803b79274c257a0c1f07a76a26820f1
Antonio Glaybson <gleybsonmelo998@gmail.com>
2023-03-06 12:20:09 -0300

    added product edit

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require("../models/Product");
 
 
-exports.getAddProduct = (req, res, next) => {
+exports.getAddProduct = (req, res) => {
     // res.sendFile(path.join(rootDir,"views", "add-product.html"));
     res.render("admin/add-product",  {
         pageTitle:"Add Product",
@@ -11,10 +11,30 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res) => {
     let { title, imageUrl, price, description } = req.body;
-    let product = new Product(title, imageUrl, price, description);
+    let product = new Product(null, title, imageUrl, price, description);
     product.save();
     res.redirect("/");
 }
+
+exports.getEditProduct = (req, res) => {
+    
+    const productId = req.params.id;
+    Product.findById(productId, product => {
+        if(product.length == 0) return res.redirect("/admin/products")
+        res.render("admin/edit-product",{
+            pageTitle:"Edit Product",
+            path:"/admin/products",
+            product
+        });
+    });
+}
+
+exports.postEditProduct = (req, res) => {
+    const {productId, updatedTitle, updatedImageUrl, updatedPrice, updatedDescription} = req.body;
+    const product = new Product(productId, updatedTitle, updatedImageUrl, updatedPrice, updatedDescription);
+    product.save();
+    res.redirect("/admin/products");
+};
 
 exports.getProducts = (req, res) => {
     Product.findAll(products => {
@@ -24,12 +44,4 @@ exports.getProducts = (req, res) => {
             products
         })
     })
-}
-
-exports.getEditProduct = (req, res) => {
-    res.render("admin/edit-product",{
-        pageTitle:"Edit Product",
-        path:""
-    }
-    );
 }
--- models/Product.js
+++ models/Product.js
@@ -19,8 +19,8 @@ const getPrductsFromFile = callback => {
 }
 
 module.exports = class Product{
-    constructor(title, imageUrl, price, description){
-        this.id = 0;
+    constructor(id, title, imageUrl, price, description){
+        this.id = id;
         this.title = title;
         this.slug = slugify(title, {lower:true});
         this.imageUrl = imageUrl;
@@ -28,25 +28,43 @@ module.exports = class Product{
         this.description = description;
     }
     save(){
-        this.id = v4();
         getPrductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), error => {
-                if(error)
-                    console.log(error)
-            });
+
+            if(this.id){
+                let existingProductIndex = products.findIndex(prod => prod.id === this.id);
+                console.log(existingProductIndex)
+                
+                const updatedProducts = [...products]
+                updatedProducts[existingProductIndex] = this;
+
+                fs.writeFile(p, JSON.stringify(updatedProducts), error => {
+                    if(error)
+                        console.log(error)
+                });
+                
+            }
+            else{
+                this.id = v4();
+                products.push(this);
+                fs.writeFile(p, JSON.stringify(products), error => {
+                    if(error)
+                        console.log(error)
+                });
+            }
         })
 
     }
     static findAll(callback){
         getPrductsFromFile(callback);
     }
+
     static findBySlug(slug, callback){
         getPrductsFromFile(products => {
             const product = products.find(p => p.slug == slug);
             callback(product);
         })
     }
+
     static findById(id, callback){
         getPrductsFromFile(products => {
             const product = products.find(prod => prod.id === id);
